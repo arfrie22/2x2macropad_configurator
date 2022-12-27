a6c75b95fcbf96474e913350d3ee41ff1e1ea12a
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2022-12-27 18:23:46 -0500

    Started to create dmg packager

---
+++ scripts/package_dmg.sh
@@ -0,0 +1,13 @@
+[[ -f Macropad-Configurator-Installer.dmg ]] && rm Macropad-Configurator-Installer.dmg
+
+create-dmg \
+  --volname "Macropad Configurator Installer" \
+  --background "../assets/installer/Background.png" \
+  --window-pos 200 120 \
+  --window-size 800 400 \
+  --icon-size 100 \
+  --icon "Macropad Configurator.app" 200 190 \
+  --hide-extension "Macropad Configurator.app" \
+  --app-drop-link 600 185 \
+  "Macropad-Configurator-Installer.dmg" \
+  "../target/debug/bundle/osx/"
