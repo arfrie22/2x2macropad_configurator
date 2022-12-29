6f961313f25a3394a719dbdd9c3cfe196389a9cf
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2022-12-28 21:57:21 -0500

    Tested loops and found the issue was in the firmware

--- src/bin/compile_tester.rs
+++ src/bin/compile_tester.rs
@@ -49,12 +49,29 @@ fn main() {
             //     delay: Some(Duration::from_millis(200)),
             // });
 
+            // mac.add_frame(MacroFrame {
+            //     action: macro_parser::ActionType::String(
+            //         "This is a test, lmao".to_string(),
+            //         Some(Duration::from_millis(100)),
+            //     ),
+            //     delay: None,
+            // });
+
             mac.add_frame(MacroFrame {
-                action: macro_parser::ActionType::String(
-                    "This is a test, lmao".to_string(),
-                    Some(Duration::from_millis(100)),
-                ),
-                delay: None,
+                action: ActionType::Loop(vec![
+                    MacroFrame {
+                        action: ActionType::Chord(vec![Keyboard::A, Keyboard::B], Some(Duration::from_millis(30))),
+                        delay: Some(Duration::from_millis(200)),
+                    },
+                    MacroFrame {
+                        action: ActionType::String(
+                            "This is a test, lmao".to_string(),
+                            Some(Duration::from_millis(100)),
+                        ),
+                        delay: None,
+                    },
+                ], 3),
+                delay: Some(Duration::from_millis(200)),
             });
 
             let macro_data = mac.pack().unwrap();
@@ -63,10 +80,13 @@ fn main() {
             macropad_wrapper::set_led_effect(&d, LedEffect::Rainbow).unwrap();
             macropad_wrapper::set_led_effect_period(&d, 5.0).unwrap();
 
-            // println!("In\n{:?}", macro_data);
+            println!("Macro data: {:?}", parse_macro(&macro_data));
+            println!("In\n{:?}", macro_data);
             macropad_wrapper::clear_macro(&d, 4).unwrap();
             macropad_wrapper::set_macro(&d, 4, &macro_data).unwrap();
-            // println!("Out\n{:?}", macropad_wrapper::get_macro(&d, 4).unwrap());
+            let mut macro_d_2 = [0u8; 4092];
+            macropad_wrapper::set_macro(&d, 4, &macro_d_2).unwrap();
+            println!("Out\n{:?}", macropad_wrapper::get_macro(&d, 4).unwrap());
             // macro_data[1] = 0x00;
             macropad_wrapper::validate_macro(&d, 4, &macro_data).unwrap();
 
